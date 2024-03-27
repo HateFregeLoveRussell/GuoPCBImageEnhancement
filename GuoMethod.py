85d49a8c0fdf57670b417b12e4039dd2109f1e18
HateFregeLoveRussell <HateFregeLoveRussell@outlook.com>
2024-03-27 15:37:43 -0400

    Major Convolution Optimization. Convolving kernel with itself before convolving with the image. Removing the need for multithreading.

--- GuoMethod.py
+++ GuoMethod.py
@@ -5,15 +5,11 @@ import cv2
 
 import cv2
 import numpy as np
-import csv
-import time
-from datetime import datetime
 import cProfile
 import pstats
 import os
 import matplotlib.pyplot as plt
 from scipy.signal import convolve
-from concurrent.futures import ProcessPoolExecutor
 
 sigma = 3
 a=0.66
@@ -22,10 +18,6 @@ a_p = (1 - np.exp(-p_values / a)) / (1 + np.exp(-p_values / a))
 
 super_p_values = np.arange(-2*sigma ,2*sigma + 1)
 super_a_p = convolve(a_p, a_p, mode='full')
-print(f'super_p_values shape: {super_p_values.shape}')
-print(f'super_p_values = {super_p_values}')
-print(f'super_a_p shape: {super_a_p.shape}')
-print(f'super_a_p = {super_a_p}')
 
 thetaList = [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4]
 
@@ -61,74 +53,12 @@ def bilinear_interpolate_vectorized(image, x, y):
     return wa * Ia + wb * Ib + wc * Ic + wd * Id
 
 
-def sigmoidSingular(neighborhood, theta):
-    x,y =  sigma + 1, sigma + 1
-    x_new = x + p_values * np.cos(theta)
-    y_new = y + p_values * np.sin(theta)
-    # print(f'neighborhood shape: {neighborhood.shape}, theta: {theta}')
-    # print(f'x_new: {x_new}, y_new: {y_new}')
-    f_p = bilinear_interpolate_vectorized(neighborhood, x_new, y_new)
-
-    output = np.dot(f_p,a_p)
-    return output
-
 def vectorSigmoid(neighborhood):
     x,y = 2*sigma + 1, 2*sigma + 1
     x_new = x + np.outer(np.cos(thetaList),super_p_values)
     y_new = y + np.outer(np.sin(thetaList),super_p_values)
     f_p = bilinear_interpolate_vectorized(neighborhood, x_new, y_new)
     return np.dot(f_p,super_a_p)
-
-
-def listSigmoid(neighborhood):
-    x, y = sigma + 1, sigma + 1
-
-    # Calculate x_new and y_new for all theta values simultaneously
-    # Shapes are expanded to (len(thetaList), len(p_values)) to perform the calculations for all thetas at once
-    x_new = x + np.outer(np.cos(thetaList), p_values)
-    y_new = y + np.outer(np.sin(thetaList), p_values)
-
-    # Perform the bilinear interpolation for all thetas and their corresponding p_values in a single call
-    # f_p will have a shape of (len(thetaList), len(p_values))
-    f_p = bilinear_interpolate_vectorized(neighborhood, x_new, y_new)
-
-    # Compute the activation 'a_p' for each 'p_value', this does not depend on theta and is 1D
-    # Now, for the dot product, since f_p is 2D (theta x p_values) and a_p is 1D, numpy will handle this as
-    # a row-wise dot product, giving us the output for each theta.
-    outputs = np.dot(f_p, a_p)
-
-    return outputs
-
-def SecondPassSigmoidTransformPerTheta(theta, output_image, pad_width, height, width):
-    print(f'Processing theta {theta}')
-    padded_output_image = cv2.copyMakeBorder(output_image, pad_width + 1, pad_width + 1, pad_width + 1, pad_width + 1,
-                                             cv2.BORDER_REPLICATE)
-    final_output_image = np.zeros((height, width))
-
-    # Buffer for rows of the padded output image, adapted for grayscale
-    row_buffer = np.zeros((2 * sigma + 3, padded_output_image.shape[1]))
-
-    # Initial population of the row buffer
-    row_buffer[:2 * sigma + 3, :] = padded_output_image[:2 * sigma + 3, :]
-
-    for y in range(height):
-        if y % 10 == 0:  # Print progress every 10 lines
-            print(f'Convolution progress for theta {theta}: {round(y / height * 100, 2)}%')
-
-        for x in range(width):
-            x_sample, y_sample = x + pad_width + 1, y + pad_width + 1
-
-            # Extract the neighborhood from the row_buffer instead of the padded_output_image
-            neighborhood = row_buffer[:, x_sample - sigma - 1:x_sample + sigma + 2]
-
-            final_output_image[y, x] = sigmoidSingular(neighborhood, theta)
-
-        # Update the row buffer for the next row
-        if y < height - 1:  # Prevent index out of bounds on the last iteration
-            row_buffer[:-1, :] = row_buffer[1:, :]
-            row_buffer[-1, :] = padded_output_image[2 * sigma + 3 + y, :]
-
-    return theta, final_output_image
 
 def DoubleSigmoidSinglePassTransform(image, betaList):
     height, width = image.shape
@@ -179,82 +109,12 @@ def DoubleSigmoidSinglePassTransform(image, betaList):
         mask = indices_of_max_abs == i
         sigmoid_image[mask] = output_images[i][mask]
 
-    visualize('Intermediates', f'Sigmoid Image', sigmoid_image)
+    # visualize('Intermediates', f'Sigmoid Image', sigmoid_image)
     output_image = np.zeros((len(betaList), height, width))
     for i, beta in enumerate(betaList):
         output_image[i] = image - beta * sigmoid_image
     return output_image
 
-def DoubleSigmoidTransform(image, betaList):
-    height, width = image.shape
-    pad_width = sigma
-    padded_image = cv2.copyMakeBorder(image, pad_width + 1, pad_width + 1, pad_width + 1, pad_width + 1,
-                                      cv2.BORDER_REFLECT)
-    output_images = np.zeros((len(thetaList), height, width))
-
-    # Buffer for rows of the padded image, adjusted for a grayscale image
-    row_buffer = np.zeros((2 * sigma + 3, padded_image.shape[1]))
-
-    # Initial population of the row buffer
-    row_buffer[:2 * sigma + 3, :] = padded_image[:2 * sigma + 3, :]
-
-    print('FIRST PASS')
-    for y in range(height):
-        if y % 10 == 0:
-            print(f'Convolution progress: {round(y / height * 100, 2)}%')
-
-        for x in range(width):
-            # Coordinates in the padded image
-            x_sample, y_sample = x + pad_width + 1, y + pad_width + 1
-
-            # Extract the neighborhood from the row_buffer
-            neighborhood = row_buffer[:, x_sample - sigma - 1:x_sample + sigma + 2]
-
-            outputs = listSigmoid(neighborhood)
-            for i, output in enumerate(outputs):
-                output_images[i, y, x] = output
-
-        # After processing each row, update the row buffer for the next row
-        if y < height - 1:  # Check to prevent index out of bounds on the last iteration
-            # Scroll the row buffer: discard the top row and append the next row from the padded_image
-            row_buffer[:-1, :] = row_buffer[1:, :]
-            # Add the new bottom row to the buffer
-            row_buffer[-1, :] = padded_image[2 * sigma + 3 + y, :]
-
-    print('SECOND PASS')
-    final_output_images = np.zeros_like(output_images)
-    # Use ProcessPoolExecutor to parallelize the second pass
-    with ProcessPoolExecutor() as executor:
-        futures = [
-            executor.submit(SecondPassSigmoidTransformPerTheta, theta, output_images[i], pad_width, height, width)
-            for i, theta in enumerate(thetaList)]
-        for future in futures:
-            theta, processed_image = future.result()
-            index = thetaList.index(theta)
-            final_output_images[index] = processed_image
-            # visualize('Intermediates', f'DoubleSigmoid{round(theta, 2)}', processed_image)
-            # visualize('Intermediates', f'SingleSigmoid{round(theta, 2)}', output_images[index])
-
-    # Calculate the absolute values of the images
-    abs_images = np.abs(final_output_images)
-
-    # Find the indices of the maximum values along the image dimension
-    indices_of_max_abs = np.argmax(abs_images, axis=0)
-
-    # Initialize an empty array to store the combined image
-    sigmoid_image = np.zeros_like(final_output_images[0])
-
-    # Iterate over each image index to populate the combined image
-    for i in range(final_output_images.shape[0]):
-        # Use the indices to mask out and select only the maximum values for each position
-        mask = indices_of_max_abs == i
-        sigmoid_image[mask] = final_output_images[i][mask]
-
-    # visualize('Intermediates', f'Sigmoid Image', sigmoid_image)
-    output_image = np.zeros((len(betaList), height, width))
-    for i, beta in enumerate(betaList):
-        output_image[i] = image - beta*sigmoid_image
-    return output_image
 def visualize(folder, name, image, vmin=-600, vmax=1200):
     # Visualize the array as a heat map
     plt.figure(figsize=(8, 6))
